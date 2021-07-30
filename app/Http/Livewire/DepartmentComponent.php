c6bd9ca70c5b2d6e4b3fd7858c876c8c916eb995
Tony Nguyen <nguyencuonghut5@gmail.com>
2021-07-30 23:09:37 +0700

    Add search function for Department table

--- app/Http/Livewire/DepartmentComponent.php
+++ app/Http/Livewire/DepartmentComponent.php
@@ -7,9 +7,18 @@ use Livewire\Component;
 
 class DepartmentComponent extends Component
 {
+    public $search;
+
+    public function mount()
+    {
+        $this->search = '';
+    }
     public function render()
     {
-        $departments = Department::all();
+        $departments = Department::where('id', 'like', '%'.$this->search.'%')
+                                ->orWhere('name', 'like', '%'.$this->search.'%')
+                                ->orderBy('id', 'desc')
+                                ->get();
         return view('livewire.department-component', ['departments' => $departments])->layout('layouts.base');
     }
 }
