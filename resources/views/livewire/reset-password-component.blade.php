614bb03f25fafb97973cbf68b7c59615b42c1684
Tony Nguyen <nguyencuonghut5@gmail.com>
2021-08-03 11:00:06 +0700

    Fix: remove unnecessary select2 css file

--- resources/views/livewire/reset-password-component.blade.php
+++ resources/views/livewire/reset-password-component.blade.php
@@ -4,7 +4,6 @@
 @push('styles')
   <!-- Select2 -->
   <link rel="stylesheet" href="{{asset('plugins/select2/css/select2.min.css')}}">
-  <link rel="stylesheet" href="{{asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
 @endpush
 <div>
   <!-- Content Wrapper. Contains page content -->
