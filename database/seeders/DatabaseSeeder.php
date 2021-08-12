50e8bf360ebe1fcc3ea2ab86b806d76e53e2810b
Tony Nguyen <nguyencuonghut5@gmail.com>
2021-08-12 11:13:13 +0700

    Add Products migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DepartmentsTableSeeder::class,
             UsersTableSeeder::class,
+            ProductsTableSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->delete();
+
+        DB::table('products')->insert(array (
+            0 => array (
+                    'id' => 1,
+                    'code' => '1680R_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            1 => array (
+                    'id' => 2,
+                    'code' => 'H168_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            2 => array (
+                    'id' => 3,
+                    'code' => 'GT01_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            3 => array (
+                    'id' => 4,
+                    'code' => 'GT02_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            4 => array (
+                    'id' => 5,
+                    'code' => 'Max01_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            5 => array (
+                    'id' => 6,
+                    'code' => 'Max02_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            7 => array (
+                    'id' => 8,
+                    'code' => 'H128E_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            8 => array (
+                    'id' => 9,
+                    'code' => 'FS9999_20',
+                    'weight' => 20,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            9 => array (
+                    'id' => 10,
+                    'code' => '1660_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            10 => array (
+                    'id' => 11,
+                    'code' => '1110P_05',
+                    'weight' => 5,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            12 => array (
+                    'id' => 13,
+                    'code' => '1110P_20',
+                    'weight' => 20,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            14 => array (
+                    'id' => 15,
+                    'code' => '1120_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            15 => array (
+                    'id' => 16,
+                    'code' => '1100_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            16 => array (
+                    'id' => 17,
+                    'code' => '1200S_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            17 => array (
+                    'id' => 18,
+                    'code' => '1410_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            18 => array (
+                    'id' => 19,
+                    'code' => '1430_25',
+                    'weight' => 25,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+            19 => array (
+                    'id' => 20,
+                    'code' => '1100FS_25',
+                    'weight' => 20,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+            ),
+        ));
+    }
+}
