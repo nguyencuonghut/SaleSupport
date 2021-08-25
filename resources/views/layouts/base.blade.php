b7640d48c861e339644f0e974901ba12df585a53
Tony Nguyen <nguyencuonghut5@gmail.com>
2021-08-26 00:06:52 +0700

    Add favicon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title')</title>
 
+  <link rel="shortcut icon" href="{{ asset('favico.png') }}" />
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
